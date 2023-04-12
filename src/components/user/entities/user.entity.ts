b6cf644af47accab17709a69c98ff28f78b78d81
EckhartPL <mateusz.masek@wp.pl>
2023-04-12 19:47:29 +0200

    user functionality

---
+++ src/components/user/entities/user.entity.ts
@@ -0,0 +1,56 @@
+import { ArticleEntity } from 'src/components/article/entities/article.entity';
+import { CommentEntity } from 'src/components/article/entities/comment.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { UserInterface } from 'types/user';
+
+@Entity()
+export class UserEntity extends BaseEntity implements UserInterface {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({
+    length: 255,
+    nullable: false,
+    unique: true,
+  })
+  email: string;
+  @Column({
+    nullable: false,
+    type: 'varchar',
+  })
+  pwdHash: string;
+  @Column({
+    nullable: true,
+    default: null,
+    length: 255,
+  })
+  name: string;
+  @Column({
+    nullable: true,
+    default: null,
+  })
+  profilePictureUrl: string;
+  @Column({
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+  @Column()
+  hashedRt?: string;
+
+  @ManyToMany(() => ArticleEntity, (entity) => entity.likedBy)
+  @JoinTable()
+  likedArticles: ArticleEntity[];
+
+  @OneToMany(() => CommentEntity, (comment) => comment.user)
+  comments: CommentEntity[];
+
+  @OneToMany(() => ArticleEntity, (article) => article.user)
+  article?: ArticleEntity[];
+}
---
+++ src/components/user/user.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { GetCurrentUserId } from 'src/decorators';
+import { UserService } from './user.service';
+import { Response } from 'express';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Post('/upload')
+  @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(FileInterceptor('avatar'))
+  async profilePictureUpload(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() avatar: any,
+    @GetCurrentUserId() userId: string,
+  ) {
+    await this.userService.profilePictureUpload(file, userId);
+  }
+
+  @Get('/upload/:userName')
+  @HttpCode(HttpStatus.OK)
+  async getUser(@Param('userName') userName: string, @Res() res: Response) {
+    await this.userService.getUser(userName, res);
+  }
+
+  @Get('likedArticles')
+  @HttpCode(HttpStatus.OK)
+  async getLikedArticlesIds(@GetCurrentUserId() userId: string) {
+    return await this.userService.getLikedArticlesIds(userId);
+  }
+}
