920cb7ea72aa7c56ec4ef04f239074750f8c9b52
EckhartPL <mateusz.masek@wp.pl>
2023-04-12 19:45:47 +0200

    auth functionality

---
+++ src/components/auth/auth.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginResponse, Tokens } from 'types';
+import { AuthLoginDto } from './dto/auth-login.dto';
+import { RegisterDto } from '../user/dto/register.dto';
+import { RtGuard } from './guards';
+import { GetCurrentUser, GetCurrentUserId, Public } from 'src/decorators';
+import { HttpStatus } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @Post('/register')
+  @HttpCode(HttpStatus.CREATED)
+  register(@Body() registerDto: RegisterDto): Promise<Tokens> {
+    return this.authService.register(registerDto);
+  }
+
+  @Public()
+  @Post('/login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginDto: AuthLoginDto): Promise<LoginResponse> {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('/logout')
+  @HttpCode(HttpStatus.OK)
+  async logout(@GetCurrentUserId() userId: string) {
+    return this.authService.logout(userId);
+  }
+
+  @Public()
+  @UseGuards(RtGuard)
+  @Post('/refresh')
+  @HttpCode(HttpStatus.OK)
+  refreshTokens(
+    @GetCurrentUserId() userId: string,
+    @GetCurrentUser('refreshToken') refreshToken: string,
+  ): Promise<Tokens> {
+    return this.authService.refreshTokens(userId, refreshToken);
+  }
+}
